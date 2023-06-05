1df9cbebe65329b1082c06cc6b1469e90a282d5c
KHumoyunPardaboev <124547907+KHumoyunPardaboev@users.noreply.github.com>
2023-06-05 10:45:04 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+
+namespace http
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https://my-json-server.typicode.com/typicode/demo/comments";
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var httpReponse = client.GetAsync(url).Result;
+                string json = httpReponse.Content.ReadAsStringAsync().Result;
+                var jsonDates= JsonConvert.DeserializeObject <Post[]>(json);
+                foreach(var item in jsonDates)
+                {
+                    Console.WriteLine("id: [0], body: [1], postId: [2]", item.id, item.postId, item.body);
+                }
+            }
+            catch (Exception ex)
+            {
+            
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+    }
+
+    public class Post
+    {
+        public string id { get; set; }
+        public string body { get; set; }
+        public string postId { get; set; }
+    }
+}
